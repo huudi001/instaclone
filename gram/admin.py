eb9793cab422e31b1464f6cf36928a73842c7f9b
khalid hassan <abuuanas30@hotmail.com>
2017-12-03 19:31:21 +0300

    refactotring

--- gram/admin.py
+++ gram/admin.py
@@ -4,11 +4,9 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Profile, Tag, Post, Follow, Comment, Like
 
-# Register your models here.
+
 class PostAdmin(admin.ModelAdmin):
-    '''
-    Customise model in admin page
-    '''
+
     filter_horizontal = ('tags',)
 
 admin.site.register(Profile)
